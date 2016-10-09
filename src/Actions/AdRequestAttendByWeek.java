339ae0d7392d0f754b67432b2af3926e20697d61
aweng126 <13075313106@163.com>
2016-10-09 11:42:19 +0800

    主页按周显示出勤率，向前一个周，向后一个周 成功实现

--- src/Actions/AdRequestAttendByWeek.java
+++ src/Actions/AdRequestAttendByWeek.java
@@ -1,11 +1,15 @@
 package Actions;
 
+import Beans.AttendRate;
+import net.sf.json.JSONArray;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by kingwen on 2016/10/9.
@@ -16,6 +20,16 @@ public class AdRequestAttendByWeek extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+          String mgrade=req.getParameter("sign_grade");
+          String mclass=req.getParameter("sign_class");
+          String course_id=req.getParameter("course_id");
 
+         List<AttendRate> list=AttendRate.getAttendRate(mgrade,mclass,course_id);
+         resp.getWriter().write(JSONArray.fromObject(list).toString());
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(AttendRate.getAttendRate("6","2014","0002"));
     }
 }
---
+++ src/Actions/AdRequestAttendByWeekForward.java
@@ -0,0 +1,32 @@
+package Actions;
+
+import Beans.AttendRate;
+import net.sf.json.JSONArray;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by kingwen on 2016/10/9.
+ */
+@WebServlet(urlPatterns = "/requestAttendByWeekForward")
+public class AdRequestAttendByWeekForward extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String mgrade=req.getParameter("sign_grade");
+        String mclass=req.getParameter("sign_class");
+        String course_id=req.getParameter("course_id");
+        int firstweek=Integer.parseInt(req.getParameter("display_firstweek"));
+
+        List<AttendRate> list=AttendRate.getAttendRateForward(mgrade,mclass,course_id,firstweek);
+        resp.getWriter().write(JSONArray.fromObject(list).toString());
+
+    }
+}
